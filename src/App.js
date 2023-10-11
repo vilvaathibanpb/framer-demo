f0d5499e8a7f9cec9341ebe48573b7f031873356
vilvaathibanpb <vilvaathiban@gmail.com>
2023-10-12 01:25:21 +0200

    base setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./App.css";
+import { routes } from "./routes";
+import { MenuBar } from "./MenuBar";
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuBar />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+export const routes = [
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/home",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+];
