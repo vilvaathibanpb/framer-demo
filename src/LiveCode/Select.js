55e234ac09f9dc2e5a40bad8603a1df857ed00b7
vilvaathibanpb <vilvaathiban@gmail.com>
2023-10-12 11:11:07 +0200

    test

--- src/LiveCode/Select.js
+++ src/LiveCode/Select.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 
-const items = [1,2,3,4,5,6,7,8,9,10];
+const items = [1,2,3,4,5];
 const Numbers = () => {
     const [selectedID, setSelectedID] = React.useState(0);
   
